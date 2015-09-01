ab062ef87b0389e2a09ce2f862dbe92ae73ca882
crisxr7 <xavier_123321@hotmail.com>
2015-09-01 12:53:08 -0600

    HashSet Final
    Contiene el patron de diseño Factory en base a HashSet

---
+++ HashFactory.java
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------------------------
+Universidad del Valle de Guatemala
+
+Cristhian Chilel - 12417
+Cristian del Carmen - 12
+
+HashFactory.java
+Contiene el patron de diseño Factory en base a HashSet
+-----------------------------------------------------------------------------*/
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class HashFactory<E> {
+    public Set gethash(String entry){
+        if (entry.equals("HS"))
+            
+            return new HashSet(); //regresa Hashet
+        if(entry.equals("TS"))
+            return new TreeSet(); //regresa TreeSet
+        else
+            return new LinkedHashSet(); //regresa TreSet
+              
+       
+    }
+    
+}
